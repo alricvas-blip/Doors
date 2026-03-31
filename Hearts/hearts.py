4f6e594b04c5f3f3cdfd7d6dc1f72ad03cea4c26
alrickle <alric.vas@gmail.com>
2026-03-31 12:18:49 +1100

    More

--- Hearts/hearts.py
+++ Hearts/hearts.py
@@ -28,6 +28,27 @@ class Deck:
         for card in self.cards:
             card.print()
 
+    def draw(self):
+        if len(self.cards) > 0:
+            return self.cards.pop()
+        return None
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def playCard(self):
+        if len(self.hand) > 0:
+            return self.hand.pop(0)
+        return None
+
+    def displayBySuit(self, suit):
+        print(f"Cards in {suit}:")
+        for card in self.hand:
+            if card.suit == suit:
+                card.print()
+
 if __name__ == "__main__":
     # Create the deck
     my_deck = Deck()
@@ -38,3 +59,23 @@ if __name__ == "__main__":
     # Display cards in the shuffled deck
     print("Shuffled Deck:")
     my_deck.displayAll()
+
+    # Deal a player 13 cards
+    player1 = Player("Alric")
+    for i in range(13):
+        new_card = my_deck.draw()
+        if new_card:
+            player1.hand.append(new_card)
+
+    # For a player to play a card
+    print(f"\n ({player1.name}) plays a card:")
+    card_played = player1.playCard()
+    if card_played:
+        card_played.print()
+
+    # For each suit to display the list of cards the player has in that suit
+    print(f"\nCards grouped by suit for {player1.name}:")
+    player1.displayBySuit("Spades")
+    player1.displayBySuit("Hearts")
+    player1.displayBySuit("Diamonds")
+    player1.displayBySuit("Clubs")
